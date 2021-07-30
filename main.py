45ddc114ac814fb1c515fa6f366528c54968f792
AdamJHowell <adam.j.howell@gmail.com>
2021-07-30 00:09:04 -0600

    2 - Basic functionality is in place.  It can pull down a list of popular artists. The API key is stored in config.json, which is ignored for privacy, because this repo is public.

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+"""Taken from the tutorial here: https://www.dataquest.io/blog/last-fm-api-python/"""
+
+
+def lastfm_get(payload):
+    # define headers and URL
+    headers = config_json['header']  # {'user-agent': 'BigGuyWhoKills'}
+    api_key = config_json['apiKey']
+    url = config_json['url']
+
+    # Add API key and format to the payload
+    payload['api_key'] = api_key
+    payload['format'] = 'json'
+
+    return requests.get(url, headers=headers, params=payload)
+
+
+def json_print(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+if __name__ == '__main__':
+    with open('config.json') as file:
+        config_json = json.load(file)
+    # headers = {'user-agent': 'BigGuyWhoKills'}
+
+    # payload = {'api_key': api_key, 'method': 'chart.gettopartists', 'format': 'json'}
+    response = lastfm_get({'method': 'chart.gettopartists'})
+    # print(response.status_code)
+    # print(response.text)
+    json_print(response.json())
