fcdae18cfe63b78edeeb7a771f067e0927cb09a0
Adam Howell <AdamJHowell@users.noreply.github.com>
2021-07-30 09:00:41 -0600

    4 - Moved the api key from config.json to apiKey.json so the config file could be included in my public repo.
    Since this API seems to use a static header and URL, I moved those into config.json, where future static info will be kept.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 def lastfm_get(payload):
     # define headers and URL
     headers = config_json['header']  # {'user-agent': 'BigGuyWhoKills'}
-    api_key = config_json['apiKey']
+    api_key = api_key_json['apiKey']
     url = config_json['url']
 
     # Add API key and format to the payload
@@ -26,6 +26,8 @@ def json_print(obj):
 if __name__ == '__main__':
     with open('config.json') as file:
         config_json = json.load(file)
+    with open('apiKey.json') as file:
+        api_key_json = json.load(file)
     # headers = {'user-agent': 'BigGuyWhoKills'}
 
     # payload = {'api_key': api_key, 'method': 'chart.gettopartists', 'format': 'json'}
